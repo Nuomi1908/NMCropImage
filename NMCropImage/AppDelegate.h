9bc63b09d8ecdf5a849f47ec5e7c9eb787de5266
tycq <tycq@YinQuandeMac-mini.local>
2018-04-10 11:41:47 +0800

    Initial Commit

---
+++ NMCropImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NMCropImage
+//
+//  Created by zhy on 2018/4/10.
+//  Copyright © 2018年 Nuomi1908. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NMCropImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NMCropImage
+//
+//  Created by zhy on 2018/4/10.
+//  Copyright © 2018年 Nuomi1908. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
